56212b89e9d533da028f1ebe11c9f5649cc16b55
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-09-23 11:57:18 -0500

    Prueba flujo para cerrar sesion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
+import { useAuth } from './context/useAuth';
 import Register from './pages/Register';
 
 function App() {
+  const { user, loading, signOut } = useAuth();
+
+  if (loading) return <p>Cargando...</p>;
+
+  if (user) {
+    return (
+      <div>
+        <h1>¡Ya iniciaste sesión!</h1>
+        <p>Email: {user.email}</p>
+        <button onClick={signOut}>Cerrar sesión</button>
+      </div>
+    );
+  }
+
   return <Register />;
 }
 
