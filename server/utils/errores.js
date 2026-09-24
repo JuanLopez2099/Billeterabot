ca794cf8eb31ec933501a1ab8ab4891271124264
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-09-24 17:04:46 -0500

    manejo de errores completo

---
+++ server/utils/errores.js
@@ -0,0 +1,16 @@
+function errorValidacion(mensaje) {
+    const err = new Error(mensaje);
+    err.status = 400
+    return err
+}
+
+function manejoError(err, res) {
+    if(err.status) {
+        return res.status(err.status).json({ error: err.message })
+    }
+}
+
+console.err(err);
+res.status(500).json({ error: 'error interno intenta de nuevo'})
+
+module.exports = {errorValidacion, manejoError};
