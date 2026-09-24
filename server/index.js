135ebe45c5dae238fe2871a12e819a7b47ca2ebd
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-09-24 15:14:54 -0500

    se elimino el test del get

--- server/index.js
+++ server/index.js
@@ -13,16 +13,6 @@ app.get('/', (req, res) => {
   res.send('Servidor funcionando');
 });
 
-app.get('/test-db', async (req, res) => {
-  const { data, error } = await supabase
-    .from('usuarios')
-    .select('*');
-
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
-  res.json({ conectado: true, usuarios: data });
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
